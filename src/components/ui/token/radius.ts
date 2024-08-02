06ff4e76c719ff7251ecccddf38b25694c83c876
woody-morgan <woody.morgan.crafts@gmail.com>
2024-08-03 07:53:03 +0900

    feat: publish main event page

--- src/components/ui/token/radius.ts
+++ src/components/ui/token/radius.ts
@@ -1,9 +1,11 @@
-export const radiusKeyList = ["default", "sm", "lg", "none"] as const;
+export const radiusKeyList = ["default", "sm", "lg", "xl", "2xl", "none"] as const;
 export type RadiusKey = (typeof radiusKeyList)[number];
 
 export const radiusVariants = {
   default: "rounded-md",
   sm: "rounded-sm",
   lg: "rounded-lg",
+  xl: "rounded-xl",
+  "2xl": "rounded-2xl",
   none: "rounded-none",
 } satisfies Record<RadiusKey, string>;
